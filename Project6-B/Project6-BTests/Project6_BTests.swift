b9350d2c25713e542f9a775659c6e4a5bbb57501
otmanethedev <otmanethedev@gmail.com>
2021-02-22 10:13:58 +0100

    Add Project-6 AutoLayout

---
+++ Project6-B/Project6-BTests/Project6_BTests.swift
@@ -0,0 +1,33 @@
+//
+//  Project6_BTests.swift
+//  Project6-BTests
+//
+//  Created by El Ati El Adimi, Othmane on 22/02/2021.
+//
+
+import XCTest
+@testable import Project6_B
+
+class Project6_BTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
